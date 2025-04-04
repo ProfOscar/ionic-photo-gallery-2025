73f3a57cf8748a11d3bd3e3b6ac145c85a1ca866
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-04-04 11:39:55 +0200

    Inizializzato il progetto e i plugin necessari

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+defineCustomElements(window);
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
